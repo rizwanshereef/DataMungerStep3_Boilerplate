d40641f7ea08c0b95f371c779d5ad29bd2bb8ec0
rsrizwan77 <rsrizwan77@gmail.com>
2020-09-10 16:22:41 +0530

    Adding class Header

--- src/com/stackroute/datamunger/query/Header.java
+++ src/com/stackroute/datamunger/query/Header.java
@@ -2,13 +2,18 @@ package com.stackroute.datamunger.query;
 
 public class Header {
 
-	/*
-	 * This class should contain a member variable which is a String array, to hold
-	 * the headers.
-	 */
-	
-	public String[] getHeaders() {
-		return null;
-	}
+    private String headers[];
+    /*
+     * This class should contain a member variable which is a String array, to hold
+     * the headers.
+     */
 
+    public String[] getHeaders() {
+
+        return headers;
+    }
+
+    public void setHeaders(String[] headers) {
+        this.headers = headers;
+    }
 }
